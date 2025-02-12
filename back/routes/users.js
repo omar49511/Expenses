fae7af8f04bece78de76d31b2c5ef15d4219c835
omar49511 <omar.reyesz18@tectijuana.edu.mx>
2025-02-11 23:19:21 -0800

    feat: :construction: crea rutas y configura el backend

---
+++ back/routes/users.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
